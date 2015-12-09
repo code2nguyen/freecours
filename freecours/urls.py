047e3f488dacade643c9fc64b19ac69a005e4394
Nguyen Thai Vinh <code2nguyen@gmail.com>
2015-12-09 18:08:09 +0100

    modify organization of projet

--- freecours/urls.py
+++ freecours/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from . import views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-	url(r'^home/', include('freecours.home.urls')),
+	url(r'^$', views.index, name='index'),
 ]
--- freecours/views.py
+++ freecours/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.template import RequestContext, loader
 
 def index(request):
-	template = loader.get_template('home/index.html')
+	template = loader.get_template('index.html')
 	context = RequestContext(request, {})
 	return HttpResponse(template.render(context))
 # Create your views here.
